055da51f4caa8ced5a2b5be847502d9e9742e66b
WRG <88352762+WRGsite@users.noreply.github.com>
2021-09-08 09:54:14 +0800

    Add files via upload

---
+++ kirim.php
@@ -0,0 +1,20 @@
+<?php
+if($_POST['kirim']){
+	$admin = 'fatoni.pino@gmail.com'; //ganti email dg email admin (email penerima pesan)
+	
+	$nama	= htmlentities($_POST['nama']);
+	$email	= htmlentities($_POST['email']);
+	$judul	= htmlentities($_POST['judul']);
+	$pesan	= htmlentities($_POST['pesan']);
+	
+	$pengirim	= 'Dari: '.$nama.' <'.$email.'>';
+	
+	if(mail($admin, $judul, $pesan, $pengirim)){
+		echo 'SUCCESS: Pesan anda berhasil di kirim. <a href="index.php">Kembali</a>';
+	}else{
+		echo 'ERROR: Pesan anda gagal di kirim silahkan coba lagi. <a href="index.php">Kembali</a>';
+	}
+}else{
+	header("Location: index.php");
+}
+?>
